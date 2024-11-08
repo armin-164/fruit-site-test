2dcdb513d940810d0bccbf871f16237b2a18ad08
armin-164 <arminsadr164@hotmail.com>
2024-11-08 20:31:06 +0100

    Add displayAllFruits function to dynamically generate fruit cards

--- script.js
+++ script.js
@@ -6,3 +6,26 @@
     })
     .catch((error) => console.error('Error message:', error));
 })();
+
+function displayAllFruits(arr) {
+  const mainContent = document.querySelector('.main-content');
+  mainContent.innerHTML = "";
+
+  const fruitListContainer = document.createElement('div');
+  fruitListContainer.classList.add('fruit-list');
+
+  arr.forEach((fruit) => {
+    const fruitCard = document.createElement('div');
+    fruitCard.classList.add('fruit-card');
+
+    fruitCard.innerHTML = `
+    <h2 class="fruit-name">${fruit.name}</h2>
+    <img src="${fruit.imageSrc}" alt="${fruit.altDesc}">
+    <button>Read more</button>
+    `;
+
+    fruitListContainer.appendChild(fruitCard);
+  });
+
+  mainContent.appendChild(fruitListContainer);
+}
